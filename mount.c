f2b0d039aa87e2ce5af51d5b47672ff2351544c2
Robert Swiecki <robert@swiecki.net>
2017-05-27 20:19:36 +0200

    mount: less logging from mountIsDir

--- mount.c
+++ mount.c
@@ -108,7 +108,7 @@ bool mountIsDir(const char *path)
 	}
 	struct stat st;
 	if (stat(path, &st) == -1) {
-		PLOG_W("stat('%s')", path);
+		PLOG_D("stat('%s')", path);
 		return false;
 	}
 	if (S_ISDIR(st.st_mode)) {
