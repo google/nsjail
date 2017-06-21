77fbf7d2834c13ec1151fea1c51058b5d356967f
Robert Swiecki <robert@swiecki.net>
2017-06-21 17:55:20 +0200

    cpu: debug msg

--- cpu.c
+++ cpu.c
@@ -39,6 +39,7 @@ static void cpuSetRandomCpu(cpu_set_t * mask, size_t mask_size, size_t cpu_num)
 	for (;;) {
 		uint64_t n = utilRnd64() % cpu_num;
 		if (!CPU_ISSET_S(n, mask_size, mask)) {
+			LOG_D("Setting allowed CPU: %" PRIx64 " of [0-%zu]", n, cpu_num - 1);
 			CPU_SET_S(n, mask_size, mask);
 			break;
 		}
