27759b1d2e0b7ec4d27ccfc8ea79e6143d0a42f0
Robert Swiecki <robert@swiecki.net>
2017-06-21 19:21:18 +0200

    util: missing includes

--- util.c
+++ util.c
@@ -30,11 +30,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "log.h"
 
