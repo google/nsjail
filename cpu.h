e7b45b6e01758886a1d3e6d1f3f772a3ee105b80
Robert Swiecki <robert@swiecki.net>
2017-06-22 02:56:10 +0200

    cpu: correct year

--- cpu.h
+++ cpu.h
@@ -3,7 +3,7 @@
    nsjail - CPU affinity
    -----------------------------------------
 
-   Copyright 2014 Google Inc. All Rights Reserved.
+   Copyright 2017 Google Inc. All Rights Reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
