122f251efd02ab44e3ee88b1ebf297bc28ebd2de
Robert Swiecki <robert@swiecki.net>
2018-10-30 01:44:08 +0100

    mnt: simplify debug message #2

--- mnt.cc
+++ mnt.cc
@@ -90,16 +90,16 @@ static const std::string flagsToStr(uintptr_t flags) {
 	uintptr_t knownFlagMask = 0U;
 	for (const auto& i : mountFlags) {
 		if (flags & i.flag) {
+			if (!res.empty()) {
+				res.append("|");
+			}
 			res.append(i.name);
-			res.append("|");
 		}
 		knownFlagMask |= i.flag;
 	}
 
-	if (((flags & ~(knownFlagMask)) == 0) && !res.empty()) {
-		res.pop_back();
-	} else {
-		util::StrAppend(&res, "%#tx", flags & ~(knownFlagMask));
+	if (flags & ~(knownFlagMask)) {
+		util::StrAppend(&res, "|%#tx", flags & ~(knownFlagMask));
 	}
 
 	return res;
@@ -543,9 +543,9 @@ const std::string describeMountPt(const mount_t& mpt) {
 	    .append(mpt.src.empty() ? "" : mpt.src)
 	    .append(mpt.src.empty() ? "" : "' -> '")
 	    .append(mpt.dst)
-	    .append("' flags:'")
+	    .append("' flags:")
 	    .append(flagsToStr(mpt.flags))
-	    .append("' type:'")
+	    .append(" type:'")
 	    .append(mpt.fs_type)
 	    .append("' options:'")
 	    .append(mpt.options)
