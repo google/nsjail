6bd1603a18164284141f5b812311312e978e59f3
deprrous <sukhbatsuugii2004@gmail.com>
2026-08-06 16:26:43 +0800

    mnt_legacy: stage mounts before moving into place

--- mnt_legacy.cc
+++ mnt_legacy.cc
@@ -60,6 +60,7 @@ static mount_t prepareMountPoint(const nsjail::MountPt& proto) {
 	    .is_dir = true,
 	    .mounted = false,
 	    .fd = -1,
+	    .identity = {},
 	};
 
 	if (!proto.prefix_src_env().empty()) {
@@ -95,6 +96,13 @@ static mount_t prepareMountPoint(const nsjail::MountPt& proto) {
 	if (proto.noexec()) {
 		mpt.flags |= MS_NOEXEC;
 	}
+	if (proto.has_options()) {
+		for (const auto& opt : util::strSplit(proto.options(), ',')) {
+			if (mnt::isGenericMountOption(opt)) {
+				mnt::applyGenericMountOption(opt, &mpt.flags);
+			}
+		}
+	}
 
 	if (proto.has_is_dir()) {
 		mpt.is_dir = proto.is_dir();
@@ -123,115 +131,36 @@ static int tryMountRW(mount_t* mpt, const char* src, const char* dst) {
 	return res;
 }
 
-static bool createMountTarget(const std::string& path, bool is_dir) {
+static bool createStageMountpoint(const std::string& tmpdir, bool is_dir, std::string* stagepath) {
+	static uint64_t counter = 0;
+	*stagepath = tmpdir + "/stage." + std::to_string(++counter) + (is_dir ? ".d" : ".f");
 	if (is_dir) {
-		if (mkdir(path.c_str(), 0711) == -1 && errno != EEXIST) {
-			if (errno != EROFS || !util::existsAsDir(path.c_str())) {
-				PLOG_W("mkdir('%s')", path.c_str());
-				return false;
-			}
-		}
-	} else {
-		int fd =
-		    TEMP_FAILURE_RETRY(open(path.c_str(), O_CREAT | O_RDONLY | O_CLOEXEC, 0644));
-		if (fd == -1) {
-			if (errno != EROFS || !util::existsAsReg(path.c_str())) {
-				PLOG_W("open('%s', O_CREAT)", path.c_str());
-				return false;
-			}
-			return true;
-		}
-		close(fd);
-	}
-	return true;
-}
-
-static bool mountSymlink(mount_t* mpt, const std::string& dstpath) {
-	LOG_D("Creating symlink: %s -> %s", mpt->src.c_str(), dstpath.c_str());
-	if (symlink(mpt->src.c_str(), dstpath.c_str()) == -1) {
-		if (mpt->mpt->mandatory()) {
-			PLOG_E("symlink('%s' -> '%s')", mpt->src.c_str(), dstpath.c_str());
+		if (mkdir(stagepath->c_str(), 0755) == -1) {
+			PLOG_W("mkdir('%s')", stagepath->c_str());
 			return false;
 		}
-		PLOG_W("symlink('%s' -> '%s') failed (non-mandatory)", mpt->src.c_str(),
-		    dstpath.c_str());
+		return true;
 	}
-	return true;
-}
-
-static bool mountWithDynamicContent(
-    mount_t* mpt, const std::string& dstpath, const std::string& tmpdir) {
-	static uint64_t counter = 0;
-	std::string srcpath = tmpdir + "/dynamic." + std::to_string(++counter);
-	defer {
-		unlink(srcpath.c_str());
-	};
 
 	int fd = TEMP_FAILURE_RETRY(
-	    open(srcpath.c_str(), O_CREAT | O_EXCL | O_CLOEXEC | O_WRONLY, 0644));
+	    open(stagepath->c_str(), O_CREAT | O_EXCL | O_RDONLY | O_CLOEXEC, 0644));
 	if (fd == -1) {
-		PLOG_W("open('%s', O_CREAT)", srcpath.c_str());
+		PLOG_W("open('%s', O_CREAT|O_EXCL)", stagepath->c_str());
 		return false;
 	}
-
-	const auto& content = mpt->mpt->src_content();
-	bool write_ok = util::writeToFd(fd, content.data(), content.length());
 	close(fd);
-
-	if (!write_ok) {
-		LOG_W("Failed to write %zu bytes to '%s'", content.length(), srcpath.c_str());
-		return false;
-	}
-
-	mpt->flags |= MS_BIND | MS_REC | MS_PRIVATE;
-	if (tryMountRW(mpt, srcpath.c_str(), dstpath.c_str()) == -1) {
-		PLOG_W("mount('%s' -> '%s')", srcpath.c_str(), dstpath.c_str());
-		return false;
-	}
-
-	mpt->mounted = true;
 	return true;
 }
 
-static bool mountSinglePoint(mount_t* mpt, const char* newroot, const char* tmpdir) {
-	LOG_D("Mounting (legacy): %s", mnt::describeMountPt(*mpt->mpt).c_str());
-
-	const std::string dstpath = std::string(newroot) + "/" + mpt->dst;
-	std::string srcpath = mpt->src.empty() ? "none" : mpt->src;
-
-	if (!util::createDirRecursively(dstpath.c_str())) {
-		LOG_W("Failed to create parent directories for '%s'", dstpath.c_str());
-		return false;
+static void cleanupStageMountpoint(const std::string& stagepath, bool is_dir) {
+	if (stagepath.empty()) {
+		return;
 	}
-
-	if (mpt->mpt->is_symlink()) {
-		return mountSymlink(mpt, dstpath);
+	if (is_dir) {
+		rmdir(stagepath.c_str());
+		return;
 	}
-
-	if (!createMountTarget(dstpath, mpt->is_dir)) {
-		return false;
-	}
-
-	if (!mpt->mpt->src_content().empty()) {
-		return mountWithDynamicContent(mpt, dstpath, tmpdir);
-	}
-
-	if (tryMountRW(mpt, srcpath.c_str(), dstpath.c_str()) == -1) {
-		if (errno == EACCES) {
-			PLOG_W("mount('%s' -> '%s'): try 'chmod o+x' on source path",
-			    srcpath.c_str(), dstpath.c_str());
-		} else if (mpt->mpt->fstype() == "proc") {
-			PLOG_W("mount('%s' -> '%s'): procfs mount may fail if /proc has "
-			       "overmounts (e.g., /dev/null on /proc/kcore)",
-			    srcpath.c_str(), dstpath.c_str());
-		} else {
-			PLOG_W("mount('%s' -> '%s')", srcpath.c_str(), dstpath.c_str());
-		}
-		return false;
-	}
-
-	mpt->mounted = true;
-	return true;
+	unlink(stagepath.c_str());
 }
 
 static unsigned long computeRemountFlags(const mount_t& mpt, const struct statvfs& vfs) {
@@ -263,23 +192,174 @@ static unsigned long computeRemountFlags(const mount_t& mpt, const struct statvf
 	return flags;
 }
 
-bool remountPt(mnt::mount_t& mpt) {
-	if (!mpt.mounted || mpt.mpt->is_symlink()) {
+static bool remountStageMount(mount_t* mpt, const std::string& stagepath) {
+	if (mpt->dst == "/") {
 		return true;
 	}
 
 	struct statvfs vfs;
-	if (TEMP_FAILURE_RETRY(statvfs(mpt.dst.c_str(), &vfs)) == -1) {
-		PLOG_W("statvfs('%s')", mpt.dst.c_str());
+	if (TEMP_FAILURE_RETRY(statvfs(stagepath.c_str(), &vfs)) == -1) {
+		PLOG_W("statvfs('%s')", stagepath.c_str());
 		return false;
 	}
 
-	unsigned long flags = computeRemountFlags(mpt, vfs);
-	LOG_D("Remounting '%s' with flags: %s", mpt.dst.c_str(), mnt::flagsToStr(flags).c_str());
+	unsigned long flags = computeRemountFlags(*mpt, vfs);
+	LOG_D("Remounting staged '%s' with flags: %s", stagepath.c_str(),
+	    mnt::flagsToStr(flags).c_str());
 
-	if (mount(mpt.dst.c_str(), mpt.dst.c_str(), nullptr, flags, nullptr) == -1) {
-		PLOG_W("mount('%s', flags=%s)", mpt.dst.c_str(), mnt::flagsToStr(flags).c_str());
+	if (mount(stagepath.c_str(), stagepath.c_str(), nullptr, flags, nullptr) == -1) {
+		PLOG_W("mount('%s', flags=%s)", stagepath.c_str(), mnt::flagsToStr(flags).c_str());
 		return false;
+	}
+	return true;
+}
+
+static bool moveMountedStage(const std::string& stagepath, const std::string& proc_target) {
+	if (mount(stagepath.c_str(), proc_target.c_str(), nullptr, MS_MOVE, nullptr) == -1) {
+		PLOG_W("mount('%s' -> '%s', MS_MOVE)", stagepath.c_str(), proc_target.c_str());
+		return false;
+	}
+	return true;
+}
+
+static bool mountSymlink(mount_t* mpt, const mnt::resolved_dst_t& resolved) {
+	if (resolved.is_root) {
+		LOG_W("Cannot create a symlink at the root mount destination");
+		return false;
+	}
+	LOG_D("Creating symlink: %s -> %s", mpt->src.c_str(), resolved.leaf.c_str());
+	if (symlinkat(mpt->src.c_str(), resolved.dirfd, resolved.leaf.c_str()) == -1) {
+		if (mpt->mpt->mandatory()) {
+			PLOG_E("symlinkat('%s' -> '%s')", mpt->src.c_str(), resolved.leaf.c_str());
+			return false;
+		}
+		PLOG_W("symlinkat('%s' -> '%s') failed (non-mandatory)", mpt->src.c_str(),
+		    resolved.leaf.c_str());
+	}
+	return true;
+}
+
+static bool mountWithDynamicContent(
+    mount_t* mpt, const std::string& dstpath, const std::string& tmpdir, std::string* srcpath_out) {
+	static uint64_t counter = 0;
+	std::string srcpath = tmpdir + "/dynamic." + std::to_string(++counter);
+
+	int fd = TEMP_FAILURE_RETRY(
+	    open(srcpath.c_str(), O_CREAT | O_EXCL | O_CLOEXEC | O_WRONLY, 0644));
+	if (fd == -1) {
+		PLOG_W("open('%s', O_CREAT)", srcpath.c_str());
+		return false;
+	}
+
+	const auto& content = mpt->mpt->src_content();
+	bool write_ok = util::writeToFd(fd, content.data(), content.length());
+	close(fd);
+
+	if (!write_ok) {
+		LOG_W("Failed to write %zu bytes to '%s'", content.length(), srcpath.c_str());
+		return false;
+	}
+
+	mpt->flags |= MS_BIND | MS_REC | MS_PRIVATE;
+	if (tryMountRW(mpt, srcpath.c_str(), dstpath.c_str()) == -1) {
+		PLOG_W("mount('%s' -> '%s')", srcpath.c_str(), dstpath.c_str());
+		return false;
+	}
+
+	*srcpath_out = srcpath;
+	return true;
+}
+
+static bool mountSinglePoint(mount_t* mpt, int root_fd, const char* tmpdir) {
+	LOG_D("Mounting (legacy): %s", mnt::describeMountPt(*mpt->mpt).c_str());
+
+	std::string srcpath = mpt->src.empty() ? "none" : mpt->src;
+	mnt::resolved_dst_t resolved = {
+	    .dirfd = -1,
+	    .leaf = "",
+	    .is_root = false,
+	};
+	if (!mnt::resolveMountDestination(root_fd, mpt->dst, &resolved)) {
+		return false;
+	}
+	defer {
+		close(resolved.dirfd);
+	};
+
+	if (mpt->mpt->is_symlink()) {
+		return mountSymlink(mpt, resolved);
+	}
+
+	int target_fd = -1;
+	if (!mnt::createMountTargetFd(root_fd, resolved, mpt->is_dir, &target_fd, mpt->dst)) {
+		return false;
+	}
+	defer {
+		close(target_fd);
+	};
+	const std::string proc_target = mnt::procFdPath(target_fd);
+	std::string stagepath;
+	if (!createStageMountpoint(tmpdir, mpt->is_dir, &stagepath)) {
+		return false;
+	}
+	std::string dynamic_srcpath;
+	defer {
+		if (!dynamic_srcpath.empty()) {
+			unlink(dynamic_srcpath.c_str());
+		}
+		cleanupStageMountpoint(stagepath, mpt->is_dir);
+	};
+
+	if (!mpt->mpt->src_content().empty()) {
+		if (!mountWithDynamicContent(mpt, stagepath, tmpdir, &dynamic_srcpath)) {
+			return false;
+		}
+	} else if (tryMountRW(mpt, srcpath.c_str(), stagepath.c_str()) == -1) {
+		if (errno == EACCES) {
+			PLOG_W("mount('%s' -> '%s'): try 'chmod o+x' on source path",
+			    srcpath.c_str(), stagepath.c_str());
+		} else if (mpt->mpt->fstype() == "proc") {
+			PLOG_W("mount('%s' -> '%s'): procfs mount may fail if /proc has "
+			       "overmounts (e.g., /dev/null on /proc/kcore)",
+			    srcpath.c_str(), stagepath.c_str());
+		} else {
+			PLOG_W("mount('%s' -> '%s')", srcpath.c_str(), stagepath.c_str());
+		}
+		return false;
+	}
+
+	if (!remountStageMount(mpt, stagepath)) {
+		return false;
+	}
+	int stage_fd = TEMP_FAILURE_RETRY(open(stagepath.c_str(), O_PATH | O_CLOEXEC));
+	if (stage_fd < 0) {
+		PLOG_W("open('%s', O_PATH)", stagepath.c_str());
+		return false;
+	}
+	defer {
+		close(stage_fd);
+	};
+	if (!moveMountedStage(stagepath, proc_target)) {
+		return false;
+	}
+	if (!NSJAIL_SYNC_TEST_HOOK_SUFFIX("legacy.before_post_attach_capture:", mpt->dst)) {
+		return false;
+	}
+
+	if (!mnt::captureMountIdentityFromFd(stage_fd, mpt)) {
+		return false;
+	}
+	if (!NSJAIL_SYNC_TEST_HOOK_SUFFIX("legacy.after_attach:", mpt->dst)) {
+		return false;
+	}
+	mpt->mounted = true;
+	return true;
+}
+
+bool remountPt(mnt::mount_t& mpt) {
+	if (mpt.fd >= 0) {
+		close(mpt.fd);
+		mpt.fd = -1;
 	}
 	return true;
 }
@@ -319,16 +399,34 @@ std::unique_ptr<std::string> buildMountTree(nsj_t* nsj, std::vector<mnt::mount_t
 		return nullptr;
 	}
 
+	int root_fd = openat(AT_FDCWD, destdir->c_str(), O_PATH | O_DIRECTORY | O_CLOEXEC);
+	if (root_fd < 0) {
+		PLOG_E("openat('%s')", destdir->c_str());
+		return nullptr;
+	}
+	defer {
+		close(root_fd);
+	};
+
 	for (const auto& proto : nsj->njc.mount()) {
 		mount_t mpt = prepareMountPoint(proto);
 
-		if (!mountSinglePoint(&mpt, destdir->c_str(), tmpdir->c_str())) {
+		if (!mountSinglePoint(&mpt, root_fd, tmpdir->c_str())) {
 			if (mpt.mpt->mandatory()) {
 				LOG_E("Failed to mount mandatory point: %s", QC(mpt.dst));
 				return nullptr;
 			}
 		}
 		mounted_mpts->push_back(mpt);
+		if (mpt.dst == "/" && mpt.mounted) {
+			close(root_fd);
+			root_fd =
+			    openat(AT_FDCWD, destdir->c_str(), O_PATH | O_DIRECTORY | O_CLOEXEC);
+			if (root_fd < 0) {
+				PLOG_E("openat('%s') after root mount", destdir->c_str());
+				return nullptr;
+			}
+		}
 	}
 
 	if (!nsj->is_root_rw) {
